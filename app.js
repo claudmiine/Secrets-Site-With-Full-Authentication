df4fbb693847dd6c82784ac59001d8e56b9c4ec6
claudmiine <wojcieska.klaudia@gmail.com>
2020-08-15 19:09:43 +0100

    Add Environment Vars

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config(); //environment variable //always on top
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -7,9 +8,11 @@ const encrypt =require("mongoose-encryption");
 
 const app = express();
 
+console.log(process.env.API_KEY);
+
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({
+app.use(bodyParser.urlencoded({  
     extended: true
 }));
 
@@ -21,8 +24,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisourlittlesecret."
-userSchema.plugin(encrypt, {secret:secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
